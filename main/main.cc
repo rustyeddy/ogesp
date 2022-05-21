d7082f2e7694164524af03a146e14713f7c44cf5
Rusty Eddy <rusty@rustyeddy.com>
2022-05-21 10:52:03 -0700

    Connects to wifi

--- main/main.cc
+++ main/main.cc
@@ -18,13 +18,42 @@
 #include <freertos/task.h>
 #include <esp_log.h>
 
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "protocol_examples_common.h"
+
 #include "thread.hh"
 
-using namespace std::chrono;
+static char *TAG = (char *)"OGESP";
 
 extern "C" void app_main(void)
 {
-    thread_init();
+    ESP_LOGI(TAG, "[APP] Startup..");
+    ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
+    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+
+    esp_log_level_set("*", ESP_LOG_INFO);
+    esp_log_level_set("MQTT_CLIENT", ESP_LOG_VERBOSE);
+    esp_log_level_set("MQTT_EXAMPLE", ESP_LOG_VERBOSE);
+    esp_log_level_set("TRANSPORT_BASE", ESP_LOG_VERBOSE);
+    esp_log_level_set("esp-tls", ESP_LOG_VERBOSE);
+    esp_log_level_set("TRANSPORT", ESP_LOG_VERBOSE);
+    esp_log_level_set("OUTBOX", ESP_LOG_VERBOSE);
+
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /*
+     * This helper function configures Wi-Fi or Ethernet, as selected
+     * in menuconfig. Read "Establishing Wi-Fi or Ethernet
+     * Connection" section in examples/protocols/README.md for more
+     * information about this function.
+     */
+    ESP_ERROR_CHECK(example_connect());
 
     // Create a thread using deafult values that can run on any core
     auto cfg = esp_pthread_get_default_config();
